34345e3c58c4ee0ba007432c5d09b350bed38e1c
emrah karaman <ekaraman89@hotmail.com>
2021-09-15 13:01:54 +0300

    BasicGet added

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Common;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+Connection con = new Connection();
+var channel = con.GetModel();
+
+while (true)
+{
+    var b = channel.BasicGet(queue: "Test", autoAck: true);
+    if (b != null)
+    {
+        var body = b.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(message);
+    }
+}
